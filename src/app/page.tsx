05a33c4db3c15a869f46950e8dafee291efcb1e0
Валентин Пыркин <elemental-hero@yandex.by>
2026-06-20 03:12:48 +0300

    style(ui): fix layout borders and collision logic in ButtonGroup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import {} from "@/ui/components"
+import { Button, ButtonGroup } from "@/ui/components"
 
 export default function Home() {
 	return (
 		<div className="p-4 w-full sm:w-lg h-dvh mx-auto flex flex-col gap-4 items-center justify-center">
-
+			<ButtonGroup orientation="horizontal" variant="outline">
+				<Button>Button 1</Button>
+				<Button>Button 2</Button>
+				<Button>Button 3</Button>
+			</ButtonGroup>
 		</div>
 	)
 }
