ad60585400ea9c1323570f3ad47c65e41c42250d
Валентин Пыркин <elemental-hero@yandex.by>
2026-06-22 14:08:10 +0300

    style(Checkbox, Radio, Switch): refactor styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,9 @@
-"use client"
-
-import { Badge, Button } from "@/ui/components"
-import { useState } from "react";
+import {} from "@/ui/components"
 
 export default function Home() {
-	const [invisible, setInVisible] = useState(false)
-
 	return (
 		<div className="p-4 w-full sm:w-lg h-dvh mx-auto flex flex-col gap-4 items-center justify-center">
-			<Badge.Root>
-				<Button onClick={() => setInVisible(!invisible)}>Button</Button>
 
-				<Badge.Indicator
-					invisible={invisible}
-					size="sm"
-					status="warning"
-					side="top"
-					align="end"
-				>
-					3
-				</Badge.Indicator>
-			</Badge.Root>
 		</div>
 	)
 }
