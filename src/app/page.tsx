5235c3b14c4255310aa88ba1992b06191edd997e
Валентин Пыркин <elemental-hero@yandex.by>
2026-06-22 15:10:34 +0300

    style(OTPField): refactor styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import {} from "@/ui/components"
+import { OTPField } from "@/ui/components"
 
 export default function Home() {
 	return (
 		<div className="p-4 w-full sm:w-lg h-dvh mx-auto flex flex-col gap-4 items-center justify-center">
+			<OTPField.Root length={6}>
+				<OTPField.Input/>
+				<OTPField.Input/>
+				<OTPField.Input/>
 
+				<OTPField.Separator/>
+
+				<OTPField.Input/>
+				<OTPField.Input/>
+				<OTPField.Input/>
+			</OTPField.Root>
 		</div>
 	)
 }
