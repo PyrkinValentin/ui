d17efc4c3ab977ceba2ccd196d2c209c8a58df21
Валентин Пыркин <elemental-hero@yandex.by>
2026-06-23 22:18:56 +0300

    refactor(ui): remove Heading component and export Render primitive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import {} from "@/ui/components"
 export default function Home() {
 	return (
 		<div className="p-4 w-full sm:w-2xl h-dvh mx-auto flex flex-col gap-2 items-center justify-center">
-
+			
 		</div>
 	)
 }
