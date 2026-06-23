3b0ab2edaaf9462a2bf13201e14dd0a046db4edf
Валентин Пыркин <elemental-hero@yandex.by>
2026-06-23 20:07:53 +0300

    style(Tooltip,Dialog,Toast,Tabs,Switch,Spinner,Select,PreviewCard,Popover): clear transform-gpu class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import {} from "@/ui/components"
+import { Tabs } from "@/ui/components"
 
 export default function Home() {
 	return (
-		<div className="p-4 w-full sm:w-2xl h-50 mx-auto flex flex-col gap-2 items-center justify-center">
+		<div className="p-4 w-full sm:w-2xl h-dvh mx-auto flex flex-col gap-2 items-center justify-center">
 
 		</div>
 	)
